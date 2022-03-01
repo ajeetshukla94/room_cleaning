1541c1a542a3ab25c37b2059bbbfceabd4cc7765
Ajeet Shukla <98910114+ajeetshukla94@users.noreply.github.com>
2022-03-01 08:07:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,14 +26,11 @@ import time
 from openpyxl.styles import PatternFill
 from openpyxl import Workbook
 
-working_directory ="AIR_VELOCITY_REPORT\\{}"
-final_working_directory ="AIR_VELOCITY_REPORT\\{}\\{}.xlsx"
-
 app = Flask(__name__)
 app.secret_key = 'file_upload_key'
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+MYDIR = os.path.dirname(__file__)
+UPLOAD_FOLDER = "static/Report/"
 equipmet_list = ['Dispensing Booth',
 'Dispensing Scoop ( Small)','Dispensing Scoop (Large)',
 'Spatula','Spatula' ,'Manufacturing Scoop',
@@ -120,16 +117,10 @@ def submit_data():
     temp_df         = temp_df[1:] 
     temp_df.columns = new_header
     
-    temp_df.dropna(axis=1, how='all',inplace=True)
-   
-    
+    temp_df.dropna(axis=1, how='all',inplace=True)    
     wb = Workbook()
-    ws = wb.active
-    final_working_directory = "static\\Report\\{}.xlsx"
-    file_name = "Cleaning_room_report_{}".format(str(datetime.datetime.today().strftime('%d_%m_%Y')))
-    final_working_directory = final_working_directory.format(file_name)
-        
-    
+    ws = wb.active    
+    file_name = "Cleaning_room_report_{}.xlsx".format(str(datetime.datetime.today().strftime('%d_%m_%Y')))    
     end_column =temp_df.shape[1] +2   
     ws.merge_cells(start_row=2, start_column=2, end_row=2, end_column=end_column)
     ws["B" + str(2)] = 'ANNEXURE - I'
@@ -161,13 +152,15 @@ def submit_data():
         for j in range(0,len(row_data) ):   
             ws.cell(row=row, column=j+2, value=row_data[j])
         row=row+1    
-    wb.save(final_working_directory)
+        
+    final_working_directory =MYDIR + "/" + app.config['UPLOAD_FOLDER']+file_name
+    wb.save(os.path.join(app.config['UPLOAD_FOLDER'],file_name))
 
     if sent_mail:
         send_mail(subject,text,final_working_directory,file_name) 
     d = {"error":"none",
          "file_name":file_name,
-         "file_path":final_working_directory}
+         "file_path":MYDIR + "/" + app.config['UPLOAD_FOLDER']+file_name}
    
     return json.dumps(d)
 
